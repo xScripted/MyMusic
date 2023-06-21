bac191ce543302bdfef095d8e67132120e725169
ViperBite03 <layitamartin@gmail.com>
2023-06-21 02:18:09 +0200

    Volumen iconos

--- mymusic-desktop/src/components/Volume.vue
+++ mymusic-desktop/src/components/Volume.vue
@@ -3,7 +3,7 @@ import { storage, useStore, usePlayer } from 'mymusic-services'
 import { inject } from 'vue'
 
 const emitter: any = inject('emitter')
-const { updateVolume } = usePlayer(emitter)
+const { updateVolume, muteVolume } = usePlayer(emitter)
 
 const store = useStore()
 </script>
@@ -14,11 +14,21 @@ const store = useStore()
   justify-content: center;
   width: 100%;
   padding: 10px;
+
+  img{
+    width: 25px;
+    filter: invert(1);
+    margin: 2.5px;
+  }
 }
 </style>
 
 <template>
   <div id="Volume">
+    <img 
+    @click="muteVolume"
+    src="@/assets/icons/volume-mute.svg">
+
     <input
       type="range"
       min="0"
@@ -29,8 +39,10 @@ const store = useStore()
         () => {
           updateVolume()
           storage.setSettings(store.settings)
-        }
-      "
+        }"
     />
+
+    <img src="@/assets/icons/volume-high.svg">
+
   </div>
 </template>
